481e6f31472d33b8fcc6997f3de8e0a37651325a
Ben <benchadwick87@gmail.com>
2021-03-06 22:25:56 -0800

    created a separate HelloWorld class

---
+++ my-app/src/HelloWorld.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class HelloWorld extends Component {
+  render() {
+    return (
+	    <div className='App'>
+	    	<h1>Hello</h1>
+	    </div>
+    );
+  }
+
+}
+
+export default HelloWorld;
