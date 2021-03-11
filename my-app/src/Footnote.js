12f41ba54cd0f72dea3abed8e36b6c84c35c0462
Ben <benchadwick87@gmail.com>
2021-03-11 01:43:23 -0800

    added footnote for under home search bar

---
+++ my-app/src/Footnote.js
@@ -0,0 +1,13 @@
+// manage all the bootstrap for the searchbar
+import Container from "react-bootstrap/Container";
+
+
+const Footnote = () => {
+    return (
+        <Container>
+            <h5 className='my-3'>Word of the day is <a href='#'><u>Hello</u></a>.</h5>
+        </Container>
+    )
+}
+
+export default Footnote 
