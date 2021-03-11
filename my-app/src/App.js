5aea2877ea29ececd5df836bb85b591f01b1eec8
bchadwic <bchadwick@mail.greenriver.edu>
2021-03-10 22:50:58 -0800

    updating all files to move to different workstation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/App.css';
+import React from "react";
+import Header from "./Header";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <SearchBar/>
     </div>
   );
 }
