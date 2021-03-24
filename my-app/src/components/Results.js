beb8e8e2f0bb6a82947ce575c1599f9754610cfd
Ben <benchadwick87@gmail.com>
2021-03-24 00:35:03 -0700

    added scrolling feature to the definitions. Need to make the height responsive, then the styling for the definitions will be done

--- my-app/src/components/Results.js
+++ my-app/src/components/Results.js
@@ -6,7 +6,7 @@ const Results = () => {
 
     const currentWord = "Word Bubble";
     const results = [ 
-            {definition : "A web app that utilizes an open-source dictionary platform. This is used as a study tool and it is a great way to remember definitions for tests", rank: 4, user: "Ben"},
+            {definition : "A web app that utilizes an open-source dictionary platform. This is used as a study tool and it is a great way to remember definitions for tests A web app that utilizes an open-source dictionary platform. This is used as a study tool and it is a great way to remember definitions", rank: 4, user: "Ben"},
             {definition : "A place to study insert and collaborate defined study words", rank: 2, user: "Sarah"},
             {definition : "A place to post words", rank: -1, user: "Nick"},
             {definition : "Hello World", rank: -2, user: "Joe"}
@@ -14,20 +14,20 @@ const Results = () => {
 
     return (
         <div>
-        <hr />
-        <div className="Result-Root mb-3">
+            <div className="Divider mt-3"></div>
+        <div className="Result-Root">
             <div className="Results">
                 {results.map(result => ( 
                 <div className="Result-Box">
                     <div className="Result-Body"> 
                         <div className="Result-Text">
-                            <h4>{result.definition}</h4>
+                            <h4><i>{currentWord}</i> ~ {result.definition}</h4>
                         </div>
                     </div>
                     <aside className="Result-Side-Bar">
-                        <div className="Result-Rank my-auto">
+                        <div className="Result-Rank my-3">
                             <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="Up-Arrow" width="24" height="24" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg></a>
-                            <h4><b>{result.rank}</b></h4>
+                            <h4 className="my-2"><b>{result.rank}</b></h4>
                             <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="Down-Arrow" width="24" height="24" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg></a>
                         </div>
                         <div className="Result-Defined-By mx-2">
@@ -40,6 +40,7 @@ const Results = () => {
                 ))}
             </div>
         </div>
+            <div className="Divider mb-4"></div>
         </div>
     );
 };
