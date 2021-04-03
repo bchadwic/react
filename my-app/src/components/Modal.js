9498e54b71a6b142dcd8a6aff69ccab0c9c4adf0
bchadwic <bchadwick@mail.greenriver.edu>
2021-04-02 20:06:57 -0700

    added state handler to the contribution Modal, this will allow for a body to be created for a post to the api

--- my-app/src/components/Modal.js
+++ my-app/src/components/Modal.js
@@ -1,17 +1,20 @@
 import '../styles/Modal.css';
-import React from 'react';
+import React, { useState } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { unsetAdd } from '../actions';
+import { setWord, unsetAdd } from '../actions';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
 const Modal = () => {
 
     const dispatch = useDispatch();
+    const [addWord, setAddWord] = useState("");
+    const [addDefinition, setAddDefinition] = useState("");
     let setFade = false;
+
 
     useEffect(() => {
         setFade = !setFade;
@@ -22,8 +25,25 @@ const Modal = () => {
     };
 //Modal {setFade ? Modal-Fade-In : Modal-Fade-Out}
 
-    const wordInsertionHandler = (e) => {
+    const wordChangeHandler = (e) => {
+        switch(e.target.name){
+            case "addWord":
+                setAddWord(e.target.value);
+                break;
+            case "addDefinition":
+                setAddDefinition(e.target.value);
+                break;
+            default:
+                console.log("Handling unkown event, from Modal wordChangeHandler");
+                break;
+        }
+    }
+
+    const wordSubmitHandler = (e) => {
         e.preventDefault();
+        console.log("Word state is set to " + addWord);
+        console.log("Definition state is set to " + addDefinition);
+
     }
 
     return (
@@ -33,12 +53,12 @@ const Modal = () => {
                 <h4 className='Modal-Contribute'>Contribute to the dictionary!</h4>
                 <h4 className='Modal-Exit' onClick={closeHandler}>X</h4>
             </div>
-            <Form method='POST' action={wordInsertionHandler}>
+            <Form onSubmit={wordSubmitHandler}>
                     <div className="Modal-Word">
-                        <FormControl id='Modal-Word-Input' name='word' id='word' placeholder="Type the word you'd like to add"/>
+                        <FormControl id='Modal-Word-Input' name='addWord' placeholder="Type the word you'd like to add" onChange={wordChangeHandler}/>
                     </div>
                     <div className="Modal-Definition">
-                        <Form.Control className="Modal-Definition-Input" name='definition' id='definition' as='textarea' rows={5}/>
+                        <Form.Control className="Modal-Definition-Input" name='addDefinition' as='textarea' onChange={wordChangeHandler} rows={5}/>
                     </div>
                     <div className="Modal-Submit">
                         <Button className="Modal-Submit-Input" type='submit'>Submit</Button>
@@ -50,3 +70,49 @@ const Modal = () => {
 };
 
 export default Modal;
+
+
+/**
+ * 
+ 
+ App.js
+import React, { Component } from ‘react’;
+import ‘./App.css’;
+export default class App extends Component{
+    constructor(){
+        super();
+        this.state={ name:’’, email:’’ }
+    }
+
+    handleChange = event =>{
+        this.setState({ [event.target.name]:event.target.value })
+    }
+
+    handleSubmit = event =>{
+        event.preventDefault();
+        console.log(“User name : “ + this.state.name)
+        console.log(“User Email : “ + this.state.email)
+        const url =”https://jsonplaceholder.typicode.com/users/”
+        const data = { name:this.state.name, email:this.state.email }
+        fetch(url, { method: ‘POST’, // or ‘PUT’
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{ ‘Content-Type’: ‘application/json’ } })
+        .then(res => res.json())
+        .catch(error => console.error(‘Error:’, error))
+        .then(response => console.log(‘Success:’, response)); 
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+            <input type=”text” name=”name” onChange={this.handleChange} />
+            <input type=”email” name=”email” onChange={this.handleChange} />
+            <input type=”submit” value=”Add user” /> </form> 
+        )
+    }
+} 
+ * 
+ * 
+ * 
+ * 
+ */
